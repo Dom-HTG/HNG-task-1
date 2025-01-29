58ebe470477cb579ecdef3b2fb5cb86c163c94ed
Dominic <dominicdutchboy@gmail.com>
2025-01-29 15:47:00 +0100

    fixed date issue

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,5 @@
 const dummydata = {
-    email: 'dominicdutchboy@gmail.com',
-    current_datetime: new Date().toISOString(),
-    github_url: 'https://github.com/Dom-HTG/HNG-task-1.git'
+
 };
 
 export default dummydata;
--- src/handlers/handler.js
+++ src/handlers/handler.js
@@ -2,7 +2,11 @@ import dummydata from "../data/data.js";
 
 const retrieveData = async (_req, res) => {
     try {
-        res.status(200).json(dummydata);
+        res.status(200).json({
+            email: 'dominicdutchboy@gmail.com',
+            current_datetime: new Date().toISOString(),
+            github_url: 'https://github.com/Dom-HTG/HNG-task-1.git'
+        });
     } catch (e) {
         console.error(e.message);
         res.status(500).json({ message: 'Error retrieving data' });
